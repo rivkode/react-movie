a4811a0d3d977c717c791e5ba3ab18350ea8db02
jonghun <jonghuncu@gmail.com>
2024-07-22 21:41:13 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div>
-      <h1>Welcome Back React</h1>
+      <h1>Welcome Back React!!! </h1>
+      <Button text={"continue"}/>
     </div>
   );
 }
