1590a58af53ff14f838bbabbb43e19cb0590a97d
jonghun <jonghuncu@gmail.com>
2024-08-07 22:58:11 +0900

    coin tracker - 2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch(" https://api.coinpaprika.com/v1/tickers?limit=10").then(response =>
       response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, [])
 
   
 
   return <div>
-    <h1>The Coins</h1>
-    {loading ? <strong>Loading ...</strong> : null}
+    <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+    {loading ? (<strong>Loading ...</strong>) : (
+      <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name} ({coin.symbol}) : ({coin.quotes.USD.price} USD)
+          </option>
+          ))}
+      </select>
+    )}
   </div>
 }
 
